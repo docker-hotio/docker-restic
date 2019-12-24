e211dafa1ffda450e6b3e0bb1d963071b8e6c7cb
hotio <hotio@protonmail.com>
2019-12-24 10:39:42 +0100

    [CI SKIP] exit before modifying in update.sh

--- update.sh
+++ update.sh
@@ -12,11 +12,12 @@ if [[ ${1} == "checkdigests" ]]; then
 else
     version=$(curl -fsSL "https://api.github.com/repos/restic/restic/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
+    version_rclone=$(curl -fsSL "https://api.github.com/repos/ncw/rclone/releases/latest" | jq -r .tag_name | sed s/v//g)
+    [[ -z ${version_rclone} ]] && exit 1
+
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RESTIC_VERSION=.*$/ARG RESTIC_VERSION=${version}/g" {} \;
     sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
 
-    version_rclone=$(curl -fsSL "https://api.github.com/repos/ncw/rclone/releases/latest" | jq -r .tag_name | sed s/v//g)
-    [[ -z ${version_rclone} ]] && exit 1
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RCLONE_VERSION=.*$/ARG RCLONE_VERSION=${version_rclone}/g" {} \;
 
     version="${version}/${version_rclone}"
