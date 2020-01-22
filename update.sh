fb39b5d7b6ce3e3aa2f2ea428bb8448414910154
hotio <hotio@protonmail.com>
2020-01-22 12:07:53 +0100

    [CI SKIP] auto update apprise

--- update.sh
+++ update.sh
@@ -14,13 +14,17 @@ else
     [[ -z ${version} ]] && exit 1
     version_rclone=$(curl -fsSL "https://api.github.com/repos/ncw/rclone/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version_rclone} ]] && exit 1
+    version_apprise=$(curl -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -r .tag_name | sed s/v//g)
+    [[ -z ${version_apprise} ]] && exit 1
 
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RESTIC_VERSION=.*$/ARG RESTIC_VERSION=${version}/g" {} \;
     sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
 
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RCLONE_VERSION=.*$/ARG RCLONE_VERSION=${version_rclone}/g" {} \;
 
-    version="${version}/${version_rclone}"
+    find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG APPRISE_VERSION=.*$/ARG APPRISE_VERSION=${version_apprise}/g" {} \;
+
+    version="${version}/${version_rclone}/${version_apprise}"
 
     echo "##[set-output name=version;]${version}"
 fi
