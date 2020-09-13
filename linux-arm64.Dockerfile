30ff9a78fd93c322f2c20432af7bb5f0cd69f18c
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 20:13:24 +0200

    rework build

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -4,8 +4,8 @@ FROM alpine:3.11 as builder
 RUN apk add --no-cache unzip
 
 # install restic
-ARG RESTIC_VERSION
-RUN bz2file="/tmp/restic.bz2" && wget -O "${bz2file}" "https://github.com/restic/restic/releases/download/v${RESTIC_VERSION}/restic_${RESTIC_VERSION}_linux_arm64.bz2" && bunzip2 -c "${bz2file}" | dd of=/usr/local/bin/restic && chmod 755 /usr/local/bin/restic
+ARG VERSION
+RUN bz2file="/tmp/restic.bz2" && wget -O "${bz2file}" "https://github.com/restic/restic/releases/download/v${VERSION}/restic_${VERSION}_linux_arm64.bz2" && bunzip2 -c "${bz2file}" | dd of=/usr/local/bin/restic && chmod 755 /usr/local/bin/restic
 
 # install rclone
 ARG RCLONE_VERSION
@@ -18,18 +18,3 @@ ENTRYPOINT ["restic"]
 
 COPY --from=builder /usr/local/bin/restic /usr/local/bin/restic
 COPY --from=builder /usr/local/bin/rclone /usr/local/bin/rclone
-
-ARG LABEL_CREATED
-LABEL org.opencontainers.image.created=$LABEL_CREATED
-ARG LABEL_TITLE
-LABEL org.opencontainers.image.title=$LABEL_TITLE
-ARG LABEL_REVISION
-LABEL org.opencontainers.image.revision=$LABEL_REVISION
-ARG LABEL_SOURCE
-LABEL org.opencontainers.image.source=$LABEL_SOURCE
-ARG LABEL_VENDOR
-LABEL org.opencontainers.image.vendor=$LABEL_VENDOR
-ARG LABEL_URL
-LABEL org.opencontainers.image.url=$LABEL_URL
-ARG LABEL_VERSION
-LABEL org.opencontainers.image.version=$LABEL_VERSION
--- update.sh
+++ update.sh
@@ -9,13 +9,28 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "List installed packages..."
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
+    status=$((status + $?))
+    echo "Show rclone version info..."
+    docker run --rm --entrypoint="" "${2}" rclone version
+    status=$((status + $?))
+    echo "Show restic version info..."
+    docker run --rm --entrypoint="" "${2}" restic version
+    status=$((status + $?))
+    echo "Check if app works..."
+    echo "mkdir /tmp/restictest && restic init --repo /tmp/restictest && restic -r /tmp/restictest --verbose backup /var/log/ && restic -r /tmp/restictest --verbose forget --keep-last 1 --prune" > "${GITHUB_WORKSPACE}/restictest.sh"
+    docker run --rm --entrypoint="" -e RESTIC_PASSWORD=testpassphrase -v "${GITHUB_WORKSPACE}":"${GITHUB_WORKSPACE}" "${2}" sh "${GITHUB_WORKSPACE}/restictest.sh"
+    status=$((status + $?))
+    exit ${status}
 else
     version_restic=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/restic/restic/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version_restic} ]] && exit 1
     version_rclone=$(curl -fsSL "https://downloads.rclone.org/version.txt" | sed s/rclone\ v//g)
     [[ -z ${version_rclone} ]] && exit 1
-    sed -i "s/{RESTIC_VERSION=[^}]*}/{RESTIC_VERSION=${version_restic}}/g" .github/workflows/build.yml
-    sed -i "s/{RCLONE_VERSION=[^}]*}/{RCLONE_VERSION=${version_rclone}}/g" .github/workflows/build.yml
+    echo "VERSION=${version_restic}" > VERSION
+    echo "RCLONE_VERSION=${version_rclone}" >> VERSION
     version="${version_restic}/${version_rclone}"
     echo "##[set-output name=version;]${version}"
 fi
